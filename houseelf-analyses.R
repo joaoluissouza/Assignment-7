67ccbf3204f65a4333adb46d4bbda976fedd0fc3
Joao Luis Bigatao Souza <joao.bigatao.souza@gmail.com>
2017-11-06 14:34:03 -0600

    #GCgc counting function was added (ex.6.1)

--- houseelf-analyses.R
+++ houseelf-analyses.R
@@ -9,3 +9,16 @@ read.csv("./data/houseelf_earlength_dna_data_1.csv")
 
 #5
 #remote was added
+ 
+#6.1
+library(stringr)
+GC_content <- function(sequence){
+c_count <- str_count(sequence,"c")
+g_count <- str_count(sequence,"g")
+C_count <- str_count(sequence,"C")
+G_count <- str_count(sequence,"G")
+GgCc_content <- ((c_count+C_count+g_count+G_count)/str_length(sequence))*100
+return(GgCc_content)
+}
+
+
