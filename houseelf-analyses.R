14f3ba1dc7bb077a7a79e317fa2c7af5739b4918
Joao Luis Bigatao Souza <joao.bigatao.souza@gmail.com>
2017-11-06 21:13:44 -0600

    Saving for the exercise 7.6

--- houseelf-analyses.R
+++ houseelf-analyses.R
@@ -5,7 +5,7 @@
 read.csv("./data/houseelf_earlength_dna_data.csv")
 
 #4
-read.csv("./data/houseelf_earlength_dna_data_1.csv")
+data <- read.csv("./data/houseelf_earlength_dna_data_1.csv")
 
 #5
 #remote was added
@@ -21,9 +21,15 @@ GgCc_content <- ((c_count+C_count+g_count+G_count)/str_length(sequence))*100
 return(GgCc_content)
 }
 
+#7
 
 get_size_class <- function(ear_length){
    # Calculate the size class for one or more earth lengths
    ear_lengths <- ifelse(ear_length > 10, "large", "small")
    return(ear_lengths)
+}
+
+#7.4
+ID_I <- function (Individual_ID){
+  Individual_ID <- GC_content(sequence)
 }
